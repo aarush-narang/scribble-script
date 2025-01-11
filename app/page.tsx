1a57f10911cb9674fae6947b6fb246df55bcaf0b
Bailey Say <bsay@engineering.ucsb.edu>
2025-01-11 10:07:59 -0800

    home page template

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,24 @@ import { Button, useMantineColorScheme } from "@mantine/core";
 
 export default function Home() {
     const colorScheme = useMantineColorScheme();
+    // create int list with selected images
+    const [selectedImages, setSelectedImages]
 
     return (
         <div>
-            <h1>Home</h1>
-            <Button onClick={() => colorScheme.toggleColorScheme()}>swap</Button>
+            <div>
+                <Button onClick={() => colorScheme.toggleColorScheme()}>Upload</Button>
+            </div>
+            <div>
+                Image View
+            </div>
+            <div>
+                <Button onClick={() => colorScheme.toggleColorScheme()}>Extract</Button>
+            </div>
+            <br>
+            <div> // Code editor view
+
+            </div>
         </div>
     );
 }
