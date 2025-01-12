c9e1ae4bfa1731d9002a01a99dab6d6d0a02dfb5
Bailey Say <baileyasay@gmail.com>
2025-01-11 17:08:32 -0800

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
-'use client'
+'use client';
+
 import { Button, useMantineColorScheme } from "@mantine/core";
-import UploadPage from "./upload/page";
 import { useState } from "react";
 import CodeMirrorEditor from "@/components/editor/CodeMirrorEditor";
 import { useImage } from "@/components/editor/ImageContext";
-
+import UploadPage from "./upload/page";
 
 export default function Home() {
-  const colorScheme = useMantineColorScheme();
+    const colorScheme = useMantineColorScheme();
 
-  const [initialCode, setInitialCode] = useState<string>("// Write your code here...");
-  const {base64Image, setBase64Image} = useImage();
-  const [extract, setExtract] = useState<boolean>(false);
+    const [initialCode, setInitialCode] = useState<string>("// Write your code here...");
+    const { base64Image, setBase64Image } = useImage();
+    const [extract, setExtract] = useState<boolean>(false);
 
-  return (
-    <div className="ml-4">
-        <h1>Home</h1>
-        <Button onClick={() => colorScheme.toggleColorScheme()}>swap</Button>
-        <UploadPage setExtract={setExtract} setBase64Image={setBase64Image}></UploadPage>
+    return (
+        <div className="ml-4">
+            <h1>Home</h1>
+            <Button onClick={() => colorScheme.toggleColorScheme()}>swap</Button>
+            <UploadPage setExtract={setExtract} setBase64Image={setBase64Image} />
 
-      {extract && (
-      <div>
-        <h1>My CodeMirror Editor</h1>
-        <CodeMirrorEditor base64Image={base64Image} onChange={setInitialCode} />
-      </div>
-      )}
-    </div>
-  );
+            {extract && (
+                <div>
+                    <h1>My CodeMirror Editor</h1>
+                    <CodeMirrorEditor base64Image={base64Image} onChange={setInitialCode} />
+                </div>
+            )}
+        </div>
+    );
 }
