d20c93866461fed0ec2e388b524d9f878ae57d42
Aarush Narang <aarush@ucsb.edu>
2025-01-12 07:00:35 -0800

    fix error parsing

--- components/output/Output.tsx
+++ components/output/Output.tsx
@@ -4,17 +4,17 @@ import React from 'react';
 import { useMantineColorScheme } from "@mantine/core";
 import { CompilationResult } from '@/lib/types';
 
+function parseResult(result: string) {
+    return result.replace(/(\/)?(app)?(\/)?temp\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})\//g, '');
+}
+
 function Output({ compilationResult }: { compilationResult: CompilationResult}) {
     const { colorScheme } = useMantineColorScheme();
     const textColor = colorScheme === 'dark' ? 'text-white' : 'black';
     const bgColor = colorScheme === 'dark' ? 'bg-gray-800' : 'bg-gray-100';
     const erColor = colorScheme === 'dark' ? 'text-red-400' : 'text-red-700';
 
-    function parseResult(result: string) {
-        const uuid = `${result.split(' ')[0].split('/').splice(0, 2).join('/')}/`;
-        const filtered = result.replaceAll(uuid, '');
-        return filtered;
-    }
+    console.log(compilationResult);
 
     return (
         <pre className={`${bgColor} ${compilationResult.stderr ? erColor : textColor} font-mono p-4 rounded-lg`}>
